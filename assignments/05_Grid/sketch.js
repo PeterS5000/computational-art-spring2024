9e239e362ff70221ba8045d0b13d69e7e852c161
PeterS5000 <ps1944a@student.american.edu>
2024-03-04 13:34:46 -0500

    Update sketch.js

--- assignments/05_Grid/sketch.js
+++ assignments/05_Grid/sketch.js
@@ -1,5 +1,5 @@
-let numCellsWidth = 100;
-let numCellsHeight = 100;
+let numCellsWidth = 50;
+let numCellsHeight = 50;
 let cellWidth;
 let cellHeight;
 let globalOffsetX = 0; 
@@ -30,20 +30,27 @@ function drawGrid() {
 
       translate(x, y);
 
-      let hue = map(noise(x*0.01 + globalOffsetX , y*0.01 + globalOffsetY), 0, 1, 0, 200);
-      fill(hue, 150, 200);
+      let hue = map(sin(noise(x*0.01*mouseX + globalOffsetX , y*0.01*mouseY + globalOffsetY)), 0, 1, 0, 200);
+      
+      
+      fill(hue, 75, 50, 255);
       rect(0, 0, cellWidth, cellHeight);
-      let colorOffset = 20;
+      let colorOffset = 100;
 
       hue = (hue + 180 - colorOffset) % 360;
-      fill(hue, 150, 200);
+      fill(hue, 150, 100);
       noStroke();
       ellipse(cellWidth/2, cellHeight/2, cellWidth, cellHeight);
 
-      hue = (hue + colorOffset * 2) % 360;
+      hue = (hue + colorOffset * random()) % 360;
       fill(hue, 150, 200);
+      noStroke();
       ellipse(cellWidth/2, cellHeight/2, cellWidth/2, cellHeight/2);
 
+      if(mouseX > width/2 && mouseY > height/2){
+        fill (hue, 20, 50, 100);
+
+      }
       pop();
     }
   }
