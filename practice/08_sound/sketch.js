3afd42d23415b73dd4bc40eab2b96c3612cc091d
PeterS5000 <ps1944a@student.american.edu>
2024-04-17 13:40:30 -0400

    07_fractals

--- practice/08_sound/sketch.js
+++ practice/08_sound/sketch.js
@@ -3,7 +3,9 @@ let synth;
 let song;
 let LoopInterval = 1;
 let planets = []
-let numPlanets = 10;
+let numPlanet 
+let numBranches = 20;
+let count; 
 
 
 function preload(){
@@ -26,8 +28,20 @@ function setup(){
 
 function draw(){
     background(0, 0, 0, 0.1); // creates background
+    count = 0;
     fill(0,0,100);
+    push();
+    translate(random(width),  random(height));
+    rotate(frameCount * 0.02);
     ellipse(random(800), random(600), 5, 5);
+    for(let i = 0; i < numBranches; i++){
+        push();
+        let angle = map(i, 0, numBranches, 0, (2*PI));
+        rotate(angle);
+        drawBranches(50);
+        pop();
+   }
+   pop();
     
 }
 
@@ -42,4 +56,33 @@ function soundLoop(timeFromNow){
     
 
 
+}
+
+
+function drawBranches(l){
+    count++;
+
+    strokeWeight(5);
+    stroke(0, 0, 100);
+    line(0,0,0,-l);
+  
+    translate(0, -l);
+  
+    l = l * 0.6;
+  
+    if (l > 4) {
+      push();
+      rotate(radians(-50));
+      map(sin(frameCount*0.01), -1, 1, 1.01, 4);
+      
+      drawBranches(l);
+      pop();
+  
+      push();
+      rotate(radians(50));
+      drawBranches(l);
+      
+      pop();
+      console.log(count);
+    } 
 }
